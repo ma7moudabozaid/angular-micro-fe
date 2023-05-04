b3a1acfb6d1418aff2735ad9d1bc0d3d00691625
ma7moudabozaid <ma7moudabozaid@gmail.com>
2023-05-05 02:37:21 +0300

    first commit

---
+++ projects/amazing-library/src/lib/amazing-library.component.ts
@@ -0,0 +1,20 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'lib-amazing-library',
+  template: `
+    <h1>
+    amazing library
+    </h1>
+  `,
+  styles: [
+  ]
+})
+export class AmazingLibraryComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
